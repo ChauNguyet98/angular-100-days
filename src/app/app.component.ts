ed8f4b33af8de7b2c3f0fb426196e246ea0423e2
Nguyet Chau <chau.thi.nguyet@styl.solutions>
2023-04-08 15:44:37 +0700

    ViewChild and ViewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { ToggleComponent } from "./courses/toggle/toggle.component";
 
 @Component({
   selector: "app-root",
@@ -6,6 +13,29 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
+  // @ViewChild("toggle") _toggle!: ToggleComponent;
+  // @ViewChild(ToggleComponent, { static: true }) _toggle!: ToggleComponent;
+  @ViewChildren(ToggleComponent) _toggle!: QueryList<ToggleComponent>;
+
+  @ViewChild("toggleBtn", { static: true })
+  _toggleBtn!: ElementRef<HTMLButtonElement>;
+
+  @ViewChild("nameInput", { static: true })
+  _nameInput!: ElementRef<HTMLInputElement>;
+
   title = "angular-100-days";
+  name = "Angular 15";
   checked = true;
+  showLast = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this._nameInput.nativeElement.focus();
+    }, 3000);
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this._toggle);
+    this._toggle.changes.subscribe(() => console.log(this._toggle));
+  }
 }
